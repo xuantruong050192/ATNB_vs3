0e98374cd03773c4ed4a25e48acb051c60cc3bf2
NguyenTruongDev <nguyentruongdeveloper@gmail.com>
2018-05-17 06:33:53 +0700

    17_5_2018

--- EBook/src/app/app.module.ts
+++ EBook/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewbookComponent } from './components/newbook/newbook.component';
+import { SelectionComponent } from './components/selection/selection.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HomeComponent,
+    NewbookComponent,
+    SelectionComponent
   ],
   imports: [
     BrowserModule
